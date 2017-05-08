c3074097598edf74a38d1b9cecc13983c6ded7fb
Arion <Arion.Clarke@gmail.com>
2017-05-07 21:53:21 -0700

    Alarm from Static Matrix of BART Stations

--- app/src/main/java/com/forsakendragon/android/bartalarm/ChooseRouteActivity.java
+++ app/src/main/java/com/forsakendragon/android/bartalarm/ChooseRouteActivity.java
@@ -2,12 +2,98 @@ package com.forsakendragon.android.bartalarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatSpinner;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class ChooseRouteActivity extends AppCompatActivity {
+    private static final String TAG = "BART_ALARM";
+
+    private AppCompatSpinner mFromStationSpinner;
+    private AppCompatSpinner mToStationSpinner;
+    private Button mChooseRoute;
+    private Button mCancelAlarm;
+
+    private SimpleAlarm mAlarm = new SimpleAlarm();
+
+    private ArrayList<ArrayList<Integer>> mTimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_route);
+        Log.d(TAG, "ChooseRouteActivity.onCreate() being executed");
+
+        mFromStationSpinner = (AppCompatSpinner) findViewById(R.id.fromStation);
+        mFromStationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, parent.getItemAtPosition(position).toString() + " selected as From station, Position: " + position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d(TAG, "None selected for From station");
+            }
+        });
+
+        mToStationSpinner = (AppCompatSpinner) findViewById(R.id.toStation);
+        mToStationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, parent.getItemAtPosition(position).toString() + " selected as To station, Position: " + position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d(TAG, "None selected for To station");
+            }
+        });
+
+        mChooseRoute = (Button) findViewById(R.id.chooseRouteButton);
+        mChooseRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int timeToAlarm = mTimes.get(mFromStationSpinner.getSelectedItemPosition()).get(mToStationSpinner.getSelectedItemPosition());
+                Toast.makeText(ChooseRouteActivity.this,
+                               mFromStationSpinner.getSelectedItem().toString() + " to " +
+                               mToStationSpinner.getSelectedItem().toString() + " will take " + timeToAlarm,
+                               Toast.LENGTH_SHORT).show();
+                mAlarm.setAlarm(ChooseRouteActivity.this, timeToAlarm);
+
+            }
+        });
+
+        mCancelAlarm = (Button) findViewById(R.id.cancelAlarm);
+        mCancelAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAlarm.cancelAlarm();
+            }
+        });
+
+        setMatrix();
+    }
+
+    private void setMatrix() {
+        int m = 0;
+        // rows (From Station)
+        mTimes = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < mFromStationSpinner.getCount(); i++) {
+
+            // columns (To Station)
+            ArrayList<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j < mToStationSpinner.getCount(); j++) {
+                row.add(m++);
+            }
+            mTimes.add(row);
+        }
+
+        Log.d(TAG, mTimes.toString());
     }
 }
---
+++ app/src/main/java/com/forsakendragon/android/bartalarm/SimpleAlarm.java
@@ -0,0 +1,64 @@
+package com.forsakendragon.android.bartalarm;
+
+import android.content.Context;
+import android.content.Intent;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Arion on 4/25/2017.
+ */
+
+public class SimpleAlarm extends WakefulBroadcastReceiver {
+    private static final String TAG = "BART_ALARM";
+    private static final String EXTRA_ALARM_MINUTES = "EXTRA_ALARM_MINUTES";
+    private static final String EXTRA_ALARM_TYPE = "EXTRA_ALARM_TYPE";
+    // The app's AlarmManager, which provides access to the system alarm services.
+    private AlarmManager mAlarmMgr;
+    // The pending intent that is triggered when the alarm fires.
+    private PendingIntent mAlarmIntent;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        int minutesToAlarmIn = extras.getInt(EXTRA_ALARM_MINUTES);
+        String alarmType = extras.getString(EXTRA_ALARM_TYPE);
+
+        Log.d(TAG, "Alarming after " + minutesToAlarmIn + " minutes");
+        Toast.makeText(context, "Alarm!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void setAlarm(Context context, int minutesToAlarmIn) {
+        Log.d(TAG, "Alarm in " + minutesToAlarmIn + " minutes");
+
+        mAlarmMgr = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, SimpleAlarm.class);
+        Bundle extras = new Bundle();
+        extras.putInt(EXTRA_ALARM_MINUTES, minutesToAlarmIn);
+        extras.putString(EXTRA_ALARM_TYPE, "test");
+        intent.putExtras(extras);
+        mAlarmIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+//        mAlarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+//                SystemClock.elapsedRealtime() +
+//                        60 * 1000 * minutesToAlarmIn, mAlarmIntent);
+
+        // Changed from minutes to seconds for testing
+        mAlarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 1000 * minutesToAlarmIn, mAlarmIntent);
+    }
+
+    public void cancelAlarm() {
+        Log.d(TAG, "Cancelling Alarm");
+        if (mAlarmMgr!= null) {
+            mAlarmMgr.cancel(mAlarmIntent);
+            Log.d(TAG, "Alarm Cancelled");
+        }
+
+    }
+}
